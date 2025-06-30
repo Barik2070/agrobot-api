d05a466688fff06ef75b0601fdfd8a138775374e
Barik2070 <barik2070@gmail.com>
2025-06-30 19:41:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from google.oauth2.service_account import Credentials
 
 app = FastAPI()
 
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
 @app.get("/")
 def read_root():
     return {"message": "API is running"}
 
 @app.get("/test-gsheet")
 def test_gsheet():
-    creds = Credentials.from_service_account_file('/etc/secrets/credentials.json')
+    creds = Credentials.from_service_account_file('/etc/secrets/credentials.json', scopes=SCOPES)
     client = gspread.authorize(creds)
     sheet = client.open("TestSheet").sheet1
     return {"first_row": sheet.row_values(1)}
