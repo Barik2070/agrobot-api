8bb513d130b25d00d8089d7d417b913965d01b91
Barik2070 <barik2070@gmail.com>
2025-07-01 14:09:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def read_root():
 
 @app.get("/test-gsheet")
 def test_gsheet():
-    creds = Credentials.from_service_account_file('/etc/secrets/credentials.json')
+    scopes = ['https://www.googleapis.com/auth/spreadsheets']
+    creds = Credentials.from_service_account_file('/etc/secrets/credentials.json', scopes=scopes)
     client = gspread.authorize(creds)
     sheet = client.open_by_key("1akbF7aSVo4uJVQd0fFppHFm23xrAEi_s41Do2Ki-oTo").worksheet("База")
     return {"first_row": sheet.row_values(1)}
